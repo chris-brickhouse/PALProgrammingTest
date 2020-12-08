928604a2da25e39995bfbc5bf6c0a54393cc5c34
chris brickhouse <cbrickhouse@gmail.com>
2020-12-08 16:29:03 -0500

    first commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ namespace PALConsoleTest {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            NintyNineBottles();
+        }
+
+        static void NintyNineBottles() {
+            for (var i = 99; i >= 0; i--) {
+                //Console.WriteLine(i + " bottles of beer on the wall, " + i + " bottles of beer, take one down and pass it around, " + (i - 1) + " bottles of beer on the wall");
+                Console.WriteLine(i + " bottles of beer on the wall, " + i + " bottles of beer, take one down and pass it around, " + (i == 0 ? 0 : i - 1) + " bottles of beer on the wall");
+            }
         }
     }
 }
